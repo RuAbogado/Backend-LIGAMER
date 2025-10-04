546a8534269250997b10ddc81bd5f8b0b86e8d8b
Rubén Gómez Hernández <20233tn207@utez.edu.mx>
2025-10-04 11:20:32 -0600

    [Upd] Renombramiento del proyecto

---
+++ ligamer-backend/src/main/java/mx/edu/utez/sicotofbackend/SicotofBackendApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.sicotofbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicotofBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SicotofBackendApplication.class, args);
+    }
+
+}
---
+++ ligamer-backend/src/test/java/mx/edu/utez/sicotofbackend/SicotofBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.sicotofbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicotofBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
