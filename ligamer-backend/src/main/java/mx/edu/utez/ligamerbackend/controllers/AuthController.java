0eba181fcc28a0f3c9ce9d3ff3224d1e809876de
Rubén Gómez Hernández <20233tn207@utez.edu.mx>
2025-10-05 14:34:51 -0600

    [Upd] Implemente los endpoints para login y register

--- ligamer-backend/src/main/java/mx/edu/utez/ligamerbackend/controllers/AuthController.java
+++ ligamer-backend/src/main/java/mx/edu/utez/ligamerbackend/controllers/AuthController.java
@@ -1,10 +1,18 @@
 package mx.edu.utez.ligamerbackend.controllers;
 
-import mx.edu.utez.ligamerbackend.models.User;
+import mx.edu.utez.ligamerbackend.dtos.JwtAuthResponseDto;
+import mx.edu.utez.ligamerbackend.dtos.LoginDto;
+import mx.edu.utez.ligamerbackend.dtos.UserDto;
+import mx.edu.utez.ligamerbackend.services.JwtService;
 import mx.edu.utez.ligamerbackend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +25,34 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
+
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody User user) {
+    public ResponseEntity<?> registerUser(@RequestBody UserDto userDto) { // Cambiado a UserDto
         try {
-            User registeredUser = userService.registerNewUser(user);
+            userService.registerNewUser(userDto);
             return new ResponseEntity<>("¡Usuario registrado exitosamente!", HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtAuthResponseDto> loginUser(@RequestBody LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // Obtenemos el UserDetails del objeto de autenticación
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String token = jwtService.generateToken(userDetails);
+
+        return ResponseEntity.ok(new JwtAuthResponseDto(token));
     }
 }
